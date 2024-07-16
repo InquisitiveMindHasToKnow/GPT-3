fda481a5266122b08f76b31a88638716bd4190be
InquisitiveMindHasToKnow <omarraymond18@gmail.com>
2024-07-16 00:23:02 -0400

    Adds index.css file. Adds all app related Css to App.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
+import './index.css'
 
 //Links to index.html file under the public folder
 ReactDOM.render(<App/>, document.getElementById('root'))
